a5e72aaa31e3a647d4f567df825c27c7980f9f54
vleandrox <victor.alarcon.t@hotmail.com>
2024-04-16 13:21:11 -0500

    UpdateIteraciones

--- calculator/index.js
+++ calculator/index.js
@@ -1,11 +1,20 @@
-const numero1 = Number(prompt("Ingrese primer número"));
-const numero2 = Number(prompt("Ingrese segundo número"));
+// const numero1 = Number(prompt("Ingrese primer número"));
+// const numero2 = Number(prompt("Ingrese segundo número"));
 
-const suma = numero1+numero2;
-const promedio = suma / 2;
-const triple = promedio * 3;
-const resultadoMenosDiez = triple - 10;
+// const suma = numero1+numero2;
+// const promedio = suma / 2;
+// const triple = promedio * 3;
+// const resultadoMenosDiez = triple - 10;
 
-//const mensajeSuma = "El resultado de la suma es " + suma;
-const mensajeFinal = "El resultado final es " + resultadoMenosDiez;
-alert(mensajeFinal);
+// const mensajeFinal = "El resultado final es " + resultadoMenosDiez;
+// alert(mensajeFinal);
+
+const cantidad = Number(prompt("Cuantos numeros desea sumar ?"));
+let sumatoria = 0;
+
+for (let i = 1; i <= cantidad; i++) {
+    const numeroAsumar=Number(prompt("Ingrese un numero a sumar"));
+    sumatoria = sumatoria + numeroAsumar;
+}
+console.log(sumatoria);
+alert(sumatoria);
---
+++ iteraciones/iteraciones.js
@@ -0,0 +1,9 @@
+let suma = 0;
+const hastaDondeSumar = prompt("Hasta que numero quiere sumar : ");
+
+for (let i = 1; i <= hastaDondeSumar   ; i++){
+    suma = suma + i;
+}
+
+console.log(suma);
+alert(suma);
