304764789e8ab622947b0eba89c9d8fb8134dc47
vleandrox <victor.alarcon.t@hotmail.com>
2024-04-22 13:02:58 -0500

    UpdatePoo2

---
+++ example/scripts/math.js
@@ -0,0 +1,14 @@
+
+function palindrome(x){
+    const valor = Math.abs(x)
+    return valor
+}
+
+console.log(Math.round(20.5))
+console.log(Math.abs(21.7))
+console.log(Math.ceil(21.7))
+console.log(Math.floor(21.7))
+console.log(Math.round(21.7))
+console.log(Math.max(2,4,6,8,15))
+console.log(Math.min(10,11,22,23))
+console.log(Math.random())
